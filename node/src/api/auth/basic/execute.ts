db22ce8b140a137eab4fe531f5896dc2fbb9fc09
Kaushik R Bangera <kaushikrb@duck.com>
2024-02-12 19:58:42 +0530

    fix(api): auth > basic: display invalid credential error when user not found

--- node/src/api/auth/basic/execute.ts
+++ node/src/api/auth/basic/execute.ts
@@ -2,7 +2,7 @@ import { hash as hashPass } from 'bcrypt';
 
 import userDao from '../../../db/user';
 import { AUTH } from '../../../common/const';
-import { resErr } from '../../../common/error';
+import { resErr, ResponseError } from '../../../common/error';
 import { T_InData, T_OutData } from './validate';
 import getNewRefreshToken from '../../../lib/user';
 
@@ -23,9 +23,17 @@ export default async function execute(data: T_InData): Promise<T_OutData> {
       throw resErr.gen.unknown();
     }
   }
-  const userToken = await getNewRefreshToken('basic', data.email, data.pass);
-  return {
-    userId: userToken.userId,
-    refreshToken: userToken.refreshToken,
-  };
+  try {
+    const userToken = await getNewRefreshToken('basic', data.email, data.pass);
+    return {
+      userId: userToken.userId,
+      refreshToken: userToken.refreshToken,
+    };
+  } catch ( e ) {
+    if (e instanceof ResponseError) {
+      e.code = 'USER_NOT_FOUND'
+      throw resErr.auth.invalidCredential();
+    }
+    throw resErr.gen.unknown();
+  }
 }
